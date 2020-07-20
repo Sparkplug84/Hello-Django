1e270503dd9bb2973361f5b96ba7f5dc445ba6b2
Mark McClean <mcclean_mark_02@hotmail.com>
2020-07-20 09:52:50 +0000

    setup development environment

--- djamgo_todo/settings.py
+++ djamgo_todo/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 
+development = os.environ.get('DEVELOPMENT', False)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,10 +26,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '3bxa^5_xa9du(*u#f$5z*ryaxqp0-e)yvpwmq=ewu7--zqp%bc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
-# 'sparkplug84-todo-django-app.herokuapp.com', 'localhost'
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 # Application definition
 
@@ -75,16 +79,17 @@ WSGI_APPLICATION = 'djamgo_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 # postgres://upmpzzysmemfyu:a87b5a0fe80d9e9d6a3a4594ee7b2f16796e63c865ca1543761840186330c7b3@ec2-3-213-192-58.compute-1.amazonaws.com:5432/dci34dlt8rcoft
 
 # Password validation
