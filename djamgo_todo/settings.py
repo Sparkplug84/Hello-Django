7daadfe4105190e0248044e6ef465d3e00229641
Mark McClean <mcclean_mark_02@hotmail.com>
2020-07-20 10:13:24 +0000

    removed secret key

--- djamgo_todo/settings.py
+++ djamgo_todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '3bxa^5_xa9du(*u#f$5z*ryaxqp0-e)yvpwmq=ewu7--zqp%bc')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
